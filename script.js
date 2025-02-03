9f607f9f544360a61ead4d308a81f773e4b4c662
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2025-02-03 23:01:40 +0200

    Grab HTML elements by ids

---
+++ script.js
@@ -0,0 +1,7 @@
+const practiceModeButton = document.getElementById('practice-mode');
+const testModeButton = document.getElementById('test-mode');
+const testInterface = document.getElementById('test-interface');
+const questionElement = document.getElementById('question');
+const startRecordingButton = document.getElementById('start-recording');
+const transcriptElement = document.getElementById('transcript');
+const feedbackElement = document.getElementById('feedback');
