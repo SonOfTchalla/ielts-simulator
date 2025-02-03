df9aeed5ae2a5f68b271dd5a9c313d5f46327e92
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2025-02-04 00:34:54 +0200

    Add API keys

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const multer = require('multer');
 const axios = require('axios');
 const app = express();
 const upload = multer({ dest: 'uploads/' });
+
+require('dotenv').config();
+
+//API Keys
+const SPEECH_TO_TEXT_API_KEY = process.env.OPENAI_API_KEY;
+const LLM_API_KEY = process.env.STT_API_KEY;
+
